ec2e4f7e7a56a49dbeb192365a599723ec1cc300
Konovalov <fizteh.science@yandex.ru>
2024-01-10 22:18:59 +0300

    Add joystick

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include <web_interface.h>
 #include <vector>
 #include <defines.h>
-// #include <ArduinoOTA.h>
 
 robo::WebInterface ui = robo::WebInterface(
     "SheldonWiFi_2.4GHz",
@@ -106,10 +105,10 @@ robo::Quadropod robot1 = robo::Quadropod(
     {0.0,     0.0,    0.0,    0.0  }
   },
   std::vector<float>{
-    70.0*(M_PI / 180.0),
-    -70.0*(M_PI / 180.0),
-    160.0*(M_PI / 180.0),
-    70.0*(M_PI / 180.0)
+    80.0*(M_PI / 180.0),
+    -80.0*(M_PI / 180.0),
+    170.0*(M_PI / 180.0),
+    80.0*(M_PI / 180.0)
   },
   math::Matrix<float>{
     {1.0,  1.0,  1.0,  1.0 },
@@ -118,23 +117,13 @@ robo::Quadropod robot1 = robo::Quadropod(
   }
 );
 
-// robo::Limb leg1 = robo::Limb(
-//     {
-//       {37.e-3},
-//       {58.e-3},
-//       {79.e-3}
-//     },
-//     {10, 9, 8},
-//     &pwm,
-//     {
-//       {0.0},
-//       {270.0},
-//       {180.0}
-//     },
-//     "leg1"
-// );
+std::vector<robo::real> angles{0.0, 0.0, 0.0};
 
-std::vector<robo::real> angles;
+TaskHandle_t MOTION;
+TaskHandle_t UI;
+
+void loopMotion(void *pvParameters);
+void loopUi(void *pvParameters);
 
 void setup()
 {
@@ -143,136 +132,56 @@ void setup()
 
   Wire.begin(5, 4);
   robot1.begin();
-  
-  // setup OTA
-  // ArduinoOTA.begin(WiFi.localIP(),"Quadropod","password",InternalStorage);
 
-  // pwm.begin();
-  // leg1.begin();
+  xTaskCreatePinnedToCore(
+      loopMotion, /* Task function. */
+      "MOTION",   /* name of task. */
+      10000,      /* Stack size of task */
+      NULL,       /* parameter of the task */
+      1,          /* priority of the task */
+      &MOTION,    /* Task handle to keep track of created task */
+      0);         /* pin task to core 0 */
+  delay(500);
 
-  // leg1.calcServoPos(
-  //   {
-  //     {0.174},
-  //     {0.0},
-  //     {0.0}
-  //   }
-  // );
+  // create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+      loopUi,    /* Task function. */
+      "UI",      /* name of task. */
+      10000,     /* Stack size of task */
+      NULL,      /* parameter of the task */
+      1,         /* priority of the task */
+      &UI,       /* Task handle to keep track of created task */
+      1);        /* pin task to core 1 */
+  delay(500);
+}
+
+void loopMotion(void *pvParameters)
+{
+  for (;;)
+  {
+
+    robot1.calcPositions(
+        {{0.0},
+         {0.0},
+         {0.0}},
+        {{angles.at(0)},
+         {angles.at(1)},
+         {angles.at(2)}});
+
+    robot1.move();
+  }
+}
+
+void loopUi(void *pvParameters)
+{
+  for (;;)
+  {
+    ui.handle();
+
+    ui.getPlatformAngles(angles);
+  }
 }
 
 void loop()
 {
-  // ArduinoOTA.poll();
-  
-  ui.handle();
-
-  ui.getPlatformAngles(angles);
-
-  robot1.calcPositions(
-    {
-      {0.0},
-      {0.0},
-      {0.0}
-    },
-    {
-      {angles.at(0)},
-      {angles.at(1)},
-      {angles.at(2)}
-    }
-  );
-  
-  robot1.move();
-
-  // robot1.getLeg(0)->move();
-  // robot1.getLeg(1)->move();
-  // robot1.getLeg(2)->move();
-  // robot1.getLeg(3)->move();
-  
-  // robot1.getLeg(1)->calcServoPos(
-  //   {
-  //     {angles.at(0)},
-  //     {angles.at(1)},
-  //     {angles.at(2)}
-  //   }
-  // );
-
-  
-  // robot1.getLeg(0)->calcServoPos(
-  //   {
-  //     {0.0},
-  //     {116.e-3},
-  //     {58.e-3}
-  //   }
-  // );
-  // robot1.getLeg(1)->calcServoPos(
-  //   {
-  //     {0.0},
-  //     {116.e-3},
-  //     {58.e-3}
-  //   }
-  // );
-  // robot1.getLeg(2)->calcServoPos(
-  //   {
-  //     {0.0},
-  //     {116.e-3},
-  //     {58.e-3}
-  //   }
-  // );
-  // robot1.getLeg(3)->calcServoPos(
-  //   {
-  //     {0.0},
-  //     {116.e-3},
-  //     {58.e-3}
-  //   }
-  // );
-
-  
-  // robot1.getLeg(0)->calcServoPos(
-  //   {
-  //     {angles.at(0)},
-  //     {angles.at(1)},
-  //     {angles.at(2)}
-  //   }
-  // );
-  // robot1.getLeg(1)->calcServoPos(
-  //   {
-  //     {angles.at(0)},
-  //     {angles.at(1)},
-  //     {angles.at(2)}
-  //   }
-  // );
-  // robot1.getLeg(2)->calcServoPos(
-  //   {
-  //     {angles.at(0)},
-  //     {angles.at(1)},
-  //     {angles.at(2)}
-  //   }
-  // );
-  // robot1.getLeg(3)->calcServoPos(
-  //   {
-  //     {angles.at(0)},
-  //     {angles.at(1)},
-  //     {angles.at(2)}
-  //   }
-  // );
-
-  // robot1.getLeg(0)->getServo(0)->setTargetPosition(angles.at(0));
-  // robot1.getLeg(0)->getServo(1)->setTargetPosition(angles.at(1));
-  // robot1.getLeg(0)->getServo(2)->setTargetPosition(angles.at(2));
-
-  // robot1.getLeg(1)->getServo(0)->setTargetPosition(180.0-angles.at(0));
-  // robot1.getLeg(1)->getServo(1)->setTargetPosition(180.0-angles.at(1));
-  // robot1.getLeg(1)->getServo(2)->setTargetPosition(180.0-angles.at(2));
-
-  // robot1.getLeg(2)->getServo(0)->setTargetPosition(180.0-angles.at(0));
-  // robot1.getLeg(2)->getServo(1)->setTargetPosition(180.0-angles.at(1));
-  // robot1.getLeg(2)->getServo(2)->setTargetPosition(180.0-angles.at(2));
-
-  // robot1.getLeg(3)->getServo(0)->setTargetPosition(angles.at(0));
-  // robot1.getLeg(3)->getServo(1)->setTargetPosition(angles.at(1));
-  // robot1.getLeg(3)->getServo(2)->setTargetPosition(angles.at(2));
-
-  // Serial.println(" ");
-  // Serial.println("OX = "+String(angles.at(0))+" deg");
-  // Serial.println("OY = "+String(angles.at(1))+" deg");
-  // Serial.println("OZ = "+String(angles.at(2))+" deg");
 }
